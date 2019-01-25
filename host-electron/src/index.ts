f4d4d9eadd9b8364fa174cf553556c675f6b6718
Jari Pennanen <ciantic@oksidi.com>
2019-01-25 18:42:55 +0200

    ...

--- host-electron/src/index.ts
+++ host-electron/src/index.ts
@@ -1,15 +1,61 @@
 import { app, BrowserWindow } from "electron";
+import { Socket } from "net";
 
 const dev = process.env.ELECTRON_ENV && process.env.ELECTRON_ENV === "development";
+
+// On production, this promise just runs directly
+let wait = () => new Promise(res => res());
+
 if (!dev) {
+    // On production the web-service is included as part of electron application, also the gui is
+    // injected in the service url
     require("service");
 } else {
-    process.stdout.write("Development mode, you must start the service and gui manually");
+    // On development, the main window is opened once ports 3001, 3002 responds
+    process.stdout.write(
+        "Development mode, you must start the service and gui manually, waiting for ports 3001 and 3002 to rise up...\n"
+    );
+
+    /**
+     * Waits for a connection on port until resolving the promise
+     *
+     * @param param0
+     */
+    let waitConnect = ({ port }: { port: number }) => {
+        return new Promise((res, rej) => {
+            let n = 0;
+            let conn = () => {
+                if (n > 20) {
+                    // wait 20 seconds
+                    rej();
+                    return;
+                }
+                n++;
+                let c = new Socket();
+                c.connect({ port: port })
+                    .on("connect", () => {
+                        c.end();
+                        res();
+                        process.stdout.write(`Detected ${port} rise up...\n`);
+                    })
+                    .on("error", () => {
+                        // Try after 1000 milliseconds
+                        setTimeout(conn, 1000);
+                    });
+            };
+            conn();
+        });
+    };
+    wait = () => Promise.all([waitConnect({ port: 3001 }), waitConnect({ port: 3002 })]);
 }
 
 let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow() {
+    if (dev) {
+        process.stdout.write("Opening the electron window\n");
+    }
+
     // Create the browser window.
     mainWindow = new BrowserWindow({
         height: 600,
@@ -35,7 +81,14 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+    wait()
+        .then(createWindow)
+        .catch(() => {
+            process.stdout.write("Quitting: GUI and Service was not started...\n");
+            app.quit();
+        });
+});
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {

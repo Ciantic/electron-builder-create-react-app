0628ddfc99835046716224f8339645b61462f5cf
Jari Pennanen <ciantic@oksidi.com>
2019-01-26 21:13:34 +0200

    ...

--- host-electron/src/index.ts
+++ host-electron/src/index.ts
@@ -35,14 +35,11 @@ function createWindow() {
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
 // On development the ready maybe already called
-if (dev && app.isReady()) {
+if (app.isReady()) {
     createWindow();
+} else {
+    app.on("ready", createWindow);
 }
 
 // Quit when all windows are closed.
